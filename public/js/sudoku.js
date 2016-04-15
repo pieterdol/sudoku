e5dd23123bfd66c2c10cdb0a9f3aa3766f88c62b
Pieter Dol <pieterdol@gmail.com>
2016-04-15 21:58:16 +0200

    Er wordt nu een css class op result container gezet afhankelijk van resultaat

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -1,24 +1,49 @@
 submitSudoku = function(data) {
+    var resultContainer = $('#resultContainer');
     $.ajax({
         type        : "POST",
         dataType    : "json",
         url         : "index.php",
         data        : data,
         success: function(data){
-            if (typeof data.status === 'undefined' || data.status === false) {
-                var message = (data.message) ? data.message : "Something went wrong... :(";
-            } else {
-                var message = (data.message) ? data.message : "Goed bezig";
+            if (typeof resultContainer === 'undefined') {
+                return false;
             }
-            $('#resultContainer').html(message);
-            if ($('#resultContainer').is(":visible") === false) {
-                $('#resultContainer').toggle();
+            
+            // Zet bericht in result container
+            var message = (data.message) ? data.message : "Something went wrong...";
+            resultContainer.html(message);
+            
+            // Voeg juiste css class toe op basis van status
+            var status = (typeof data.status !== 'undefined' && data.status === true) ? true : false;
+            var addStatusClass = (status === true) ? 'success' : 'error';
+            var removeStatusClass = (status === true) ? 'error' : 'success';
+            addClassToElement(resultContainer, addStatusClass, removeStatusClass);
+            
+            if (resultContainer.is(":visible") === false) {
+                resultContainer.toggle();
             }
         },
-        error: function(data){
-            console.log('something went wrong');
+        error: function(){
+            addClassToElement(resultContainer, 'error', 'success');
+            resultContainer.html('Something went wrong...');
+            return false;
         }
     });
+    return true;
+};
+
+addClassToElement = function(element, classToAdd, classToRemove) {
+    if (typeof element === 'undefined') {
+        return false;
+    }
+    if (typeof classToAdd !== 'undefined' && element.hasClass(classToAdd) === false) {
+        element.addClass(classToAdd);
+    }
+    if (typeof classToRemove !== 'undefined' && element.hasClass(classToRemove) === true) {
+        element.removeClass(classToRemove);
+    }
+    return true;
 };
 
 $(document).ready(function() {	
